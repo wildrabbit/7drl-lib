0b6f7c8bec77470b3b73586fa347742a2714334b
Tania A <ithiliel@gmail.com>
2020-03-02 22:14:11 +0100

    fsm classes

---
+++ Assets/Scripts/GameData/FSM/State.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AI
+{
+    public class State : ScriptableObject
+    {
+        public List<Action> Actions;
+        public List<Transition> Transitions;
+
+        public void Update(Monster monster, float timeUnits) // TODO: Generalise monster if needed
+        {
+
+        }
+
+        public void ExecuteActions(Monster monster, float timeUnits)
+        {
+
+        }
+
+        public void EvaluateTransitions(Monster monster, float timeUnits)
+        {
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameData/FSM/Transition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AI
+{
+    [System.Serializable]
+    public class Transition
+    {
+        public Condition Condition;
+
+        public abstract bool Check(Monster monster, float timeUnits)
+        {
+            return;
+        }
+        
+    }
+
+}
